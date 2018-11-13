149a9d85bf6772473fc43f91c6d54f5d42dae863
Purab Shah <purabshah8@gmail.com>
2018-11-13 18:05:14 -0500

    create switch for router

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import LoginForm from './session_form/login_form_container';
 import SignupForm from './session_form/signup_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
@@ -7,15 +7,16 @@ const App = () => {
   const baseRender = () => <h1>Welcome to Kantrello!</h1>;
   return(
     <>
-      <Route exact path="/" render={baseRender}  />
       <nav className="splash-nav">
         <Link to="/">Kantrello</Link>
         <Link to="/login">Login</Link>
         <Link to="/signup">Signup</Link>
       </nav>
-
-      <AuthRoute exact path="/login" component={LoginForm} />
-      <AuthRoute exact path="/signup" component={SignupForm} />
+      <Switch>
+        <Route exact path="/" render={baseRender}  />
+        <AuthRoute exact path="/login" component={LoginForm} />
+        <AuthRoute exact path="/signup" component={SignupForm} />
+      </Switch>
     </>
   );
 };
