ed5c814a23260042c8a6173b2d48f58b08b96ce1
Purab Shah <purabshah8@gmail.com>
2018-11-14 13:58:00 -0500

    preparre for deployment #2

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -11,8 +11,13 @@ const Splash = () => {
           <Link className="signup-button" to="/signup">Sign Up</Link>
         </div>
       </nav>
-      <section>
-        // add splash page text, background & images
+      <section className="content">
+      <div className="content-top">
+        <div className="header-content">
+          <h1>Kantrello lets you work more collaboratively and get more done.</h1>
+        </div>
+      </div>
+      <div className="content-middle"></div>
       </section>
     </>
   );
