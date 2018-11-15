8e95dc30c305beba7411ceb0b56156da52a9edab
Purab Shah <purabshah8@gmail.com>
2018-11-15 14:32:04 -0500

    created boards table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_163952) do
+ActiveRecord::Schema.define(version: 2018_11_15_191745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.string "title", null: false
+    t.boolean "starred", default: false, null: false
+    t.integer "owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_boards_on_owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
